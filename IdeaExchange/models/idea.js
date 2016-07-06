0c8827925a05bd0cfc3b94c4e08c29209a494910
celinebrass <cjbrass@gmail.com>
2016-07-06 11:56:07 -0700

    db working

---
+++ IdeaExchange/models/idea.js
@@ -0,0 +1,19 @@
+
+var mongoose = require('mongoose');
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
+
+var ideaSchema = new mongoose.Schema({
+
+  name    :   {type: String, required: true},
+  creator :   {type: String, required: true},
+  tagline : {type: String, required: true},
+  description: {type: String, required: true},
+  tags    : [String],
+  createdOn : {type: Date, Default : Date.now},
+  likers  :   [String],
+  files   :   [String]
+});
+
+// create and export to our app
+module.exports = mongoose.model('Idea', ideaSchema);
--- IdeaExchange/routes/index.js
+++ IdeaExchange/routes/index.js
@@ -3,10 +3,33 @@ var router = express.Router();
 
 var ejs = require('ejs');
 
+var Idea = require('../models/idea');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
    res.render('index', { title: 'Express' });
+});
+
+router.get('/ideas', function(req, res, next){
+  Idea.find(function(err, ideas){
+    if (err) throw err;
+    res.json(ideas);
+  });
+});
+
+router.get('/testIdea', function(req, res, next) {
+  var newIdea = new Idea({
+    creator: "jimbob",
+    description: "owiejeowijeowifjewoifj",
+    name: "testIdea",
+    tagline: "short"
+  });
+
+  newIdea.save(function(err, idea){
+    if (err) throw err;
+    console.log("saved!");
+  });
 });
 
 module.exports = router;
