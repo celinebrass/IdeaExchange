22ee5ae82819c16c7d3019e49fbfbe05c2ebf06e
celinebrass <cjbrass@gmail.com>
2016-07-06 14:16:46 -0700

    table stuff

--- IdeaExchange/public/javascript/allIdeas.js
+++ IdeaExchange/public/javascript/allIdeas.js
@@ -11,16 +11,43 @@ function populateTableAllIdeas() {
   console.log("Looking for Ideas");
 	$.getJSON('/ideas', function(data) {
 		JSON.stringify(data);
-
 		allIdeas = $('#allIdeas').DataTable({
 			destroy: true,
 			data: data,
 			"order": [ 0, 'desc' ],
 			"pageLength" : 25,
 			columns: [
-				{ data: 'name', width:'10%' },
-				{ data: 'description', width: '10%' },
-				{ data: 'creator', width: '40%' },
+        { data: "name",
+          render :
+          function(data, type, row){
+            console.log(data);
+            return "<h2 id=tableName>" +  data + "</h2>";
+          }
+        },
+
+        {
+          data: {
+            tagline : "tagline",
+            description: "description",
+            creator: "creator"
+          },
+          render :
+          function(data, type, row){
+            console.log(data);
+            return "<p id=tagline>" +  data.tagline + "</p> <p id=description>" + data.description + "</p> <p id=author>" + data.creator + "</p>";
+          }
+
+        },
+        {
+          data: {
+            likes : "likers.length",
+            //comments: "comments.length"
+          },
+          render :
+          function(data, type, row){
+            return "<img src=/images/likeImage.png width=20px height=20px id=facebookLike> <p id=likeCount>" + likes + "</p><p id=commentCount> comments </p>";
+          }
+        },
 			],
 			language: {
 				emptyTable: function () {
@@ -28,7 +55,7 @@ function populateTableAllIdeas() {
 				}
 			},
 			drawCallback: function (settings) {
-				var pgr = $(settings.nTableWrapper).find('.dataTables_paginate')
+				var pgr = $(settings.nTableWrapper).find('.dataTables_paginate');
 				if (settings._iDisplayLength >= settings.fnRecordsDisplay()) {
 					pgr.hide();
 				}
