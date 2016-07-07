754c755ff1fabd099724f077938036eee486e4e4
jgs293 <davidjoseph.karabinos@capitalone.com>
2016-07-07 08:46:50 -0400

    add comment count on table

--- IdeaExchange/public/javascript/allIdeasBubbles.js
+++ IdeaExchange/public/javascript/allIdeasBubbles.js
@@ -153,6 +153,29 @@ function populateBubbleAllIdeas() {
 	    document.getElementById("descriptionParagraph").innerHTML = data[this.id].description + "\n";
 	    document.getElementById("tagList").innerHTML = "\n\n"+ tagString;
 			$("#bubbleModal").modal('show');
+
+		/* attach a submit handler to the form */
+			$("#bubbleCommentForm").submit(function(event) {
+
+				/* stop form from submitting normally */
+				event.preventDefault();
+				/* get the action attribute from the <form action=""> element */
+				var $form = $( this ),
+						url = $form.attr( 'action' );
+
+				/* Send the data using post with element id name and name2*/
+				var posting = $.post( '/addComment', {
+					comment: $('#bubbleCommentText').val(),
+					idea: currentRow,
+					name: getCookie("email")
+				});
+				document.getElementById("commentText").value = "";
+
+				/* Alerts the results */
+				posting.done(function( data ) {
+					alert('success');
+				});
+			});
     }
 
     function onMouseleave() {
@@ -169,6 +192,21 @@ function populateBubbleAllIdeas() {
 			document.getElementById("t"+this.id).innerHTML = data[this.id].name;
     }
 
+		function getCookie(c_name)
+		{
+		    var i,x,y,ARRcookies=document.cookie.split(";");
+
+		    for (i=0;i<ARRcookies.length;i++)
+		    {
+		        x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+		        y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+		        x=x.replace(/^\s+|\s+$/g,"");
+		        if (x==c_name)
+		        {
+		            return unescape(y);
+		        }
+		     }
+		}
     // Move d to be adjacent to the cluster node.
     function cluster(alpha) {
       return function(d) {
