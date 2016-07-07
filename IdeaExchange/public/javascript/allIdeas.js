92ac2f8342c2f27660dee16bb313dca427ab4ca6
ssbabu95 <ssb9qu@virginia.edu>
2016-07-07 11:50:17 -0400

    Fixed table images

--- IdeaExchange/public/javascript/allIdeas.js
+++ IdeaExchange/public/javascript/allIdeas.js
@@ -125,7 +125,7 @@ function populateTableAllIdeas() {
           },
           render :
           function(data, type, row){
-            return "<div id=scrollingDiv> <p id=tagline>" +  data.tagline + "</p> <p id=description>" + data.description + "</p> <p id=author> Submitted by " + data.creator + "</p></div><div id=pictureDiv> <img src=/images/likeImage.png id=facebookLike id=commentImg><p id=little> +"+ data.likers.length + "</p><img src=/images/comment.png id=commentPic id=commentImg><p id=little> +"+ data.comments.length + "</p></div>";
+            return "<div id=scrollingDiv> <p id=tagline>" +  data.tagline + "</p> <p id=description>" + data.description + "</p> <p id=author> Submitted by " + data.creator + "</p></div><div id=pictureDiv> <img src=/images/likeImage.png class=commImg id=facebookLike><p id=little> +"+ data.likers.length + "</p><img src=/images/comment.png class=commImg id=commentPic><p id=little> +"+ data.comments.length + "</p></div>";
           }
         }
 			],
